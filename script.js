9bea732a6a74ad1ca4499709c6bcc80a63b3d55b
AlessandroLembo <alessandrolembo2@gmail.com>
2023-05-02 18:20:50 +0200

    update api call with catch and then

--- script.js
+++ script.js
@@ -6,17 +6,25 @@ const app = Vue.createApp ({
       return {
         wordsList: [],
         wordsNumber: 10,
+        isLoading: false,
+        hasError: false,
+
       }
    },
 
    methods: {
    getRandomWord(){
+          this.isLoading = true;
             axios.get('https://flynn.boolean.careers/exercises/api/random/word')
             .then((response) => {
               this.wordsList.push(response.data.response);
+         }).catch(() => {
+            this.hasError = true;
+         }).then(() => {
+            this.isLoading = false;
          })
         },
-        
+         
         getSentence(){
             for(let i = 0; i < this.wordsNumber; i++) {
                 this.getRandomWord();
